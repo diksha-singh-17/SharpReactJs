b6df3cb811564edcaa9b6fd5e3a5e7655c6aeb9a
Diksha Singh <dikshasinghrathore1107@gmail.com>
2024-03-03 00:15:15 +0530

    added expense-item menu��

---
+++ sharpreact/src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h3>Expense Itemw</h3>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </div>
+  );
+}
+export default ExpenseItem;
