e9ac0b39a76a2e03171450cfbf2e4ea2a18e94d8
Diksha Singh <dikshasinghrathore1107@gmail.com>
2024-03-03 21:40:32 +0530

    added event-Handler

--- sharpreact/src/components/Expenses/ExpenseItem.js
+++ sharpreact/src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,9 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
   return (
     <div className="item">
       <ExpenseDate date={props.date} />
@@ -11,6 +14,9 @@ const ExpenseItem = (props) => {
         amount={props.amount}
         location={props.location}
       />
+      <button className="btn" onClick={clickHandler}>
+        Delete Expense
+      </button>
     </div>
   );
 };
