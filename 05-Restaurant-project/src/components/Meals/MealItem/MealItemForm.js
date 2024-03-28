9aebbfcd20d3a5db8dfc9479f2deaf4db02b4c86
Diksha Singh <dikshasinghrathore1107@gmail.com>
2024-03-29 00:23:57 +0530

    added user input box and an add button

---
+++ 05-Restaurant-project/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,23 @@
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <div>
+        <button>+ Add</button>
+      </div>
+    </form>
+  );
+};
+export default MealItemForm;
