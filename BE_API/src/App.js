5bc86d068a6ccf2780f4a10f089b0edcad00da97
Diksha Singh <dikshasinghrathore1107@gmail.com>
2024-05-08 01:25:03 +0530

    implemented errorhandling

--- BE_API/src/App.js
+++ BE_API/src/App.js
@@ -6,31 +6,50 @@ import "./App.css";
 function App() {
   const [parsedData, setParsedData] = useState([]);
   const [isloading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  let timeForFetching;
   const fetchMoviehandler = async () => {
     setIsLoading(true);
-    const fetcheddata = await fetch("https://swapi.dev/api/films/");
-    const data = await fetcheddata.json();
+    setError(null);
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setParsedData(transformedMovies);
-    setIsLoading(false);
+    timeForFetching = setInterval(async () => {
+      try {
+        const fetcheddata = await fetch("https://swapi.dev/api/films/");
+        if (!fetcheddata.ok) {
+          throw new Error("Something went wrong ....Retrying");
+        }
+        const data = await fetcheddata.json();
+
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setParsedData(transformedMovies);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error.message);
+      }
+    }, 5000);
   };
 
+  const clearMoviehandler = () => {
+    clearInterval(timeForFetching);
+    console.log("cancelling");
+  };
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviehandler}>Fetch Movies</button>
+        <button onClick={clearMoviehandler}>Clear Movies</button>
       </section>
       <section>
         {!isloading && <MoviesList movies={parsedData} />}
         {isloading && <p>Loading....</p>}
+        {isloading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
