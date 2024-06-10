df4e71caa0e9659de60af261d3f3137af8665bfd
Diksha Singh <dikshasinghrathore1107@gmail.com>
2024-06-10 21:02:18 +0530

    added delete expense functionality using firebase databse

--- expense-tracker-react/src/components/Expenses.js
+++ expense-tracker-react/src/components/Expenses.js
@@ -2,10 +2,20 @@ import React, { useEffect, useRef, useState } from "react";
 import { DATABASE_URL } from "../utils/constants";
 
 const Expenses = () => {
-  const amount = useRef(null);
-  const desc = useRef(null);
-  const category = useRef(null);
-  const [expenseData, setExpenseData] = useState();
+  // const amount = useRef(null);
+  // const desc = useRef(null);
+  // const category = useRef(null);
+  const [expenseData, setExpenseData] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [id, setId] = useState(null);
+
+  const formRefs = {
+    // id: useRef(null),
+    amount: useRef(null),
+    desc: useRef(null),
+    category: useRef(null),
+    // Add other refs as needed
+  };
 
   let url = DATABASE_URL + "expenses.json";
 
@@ -15,7 +25,6 @@ const Expenses = () => {
         return res.json();
       })
       .then((data) => {
-        console.log(data);
         const filteredData = [];
         for (let key in data) {
           filteredData.unshift({
@@ -29,45 +38,92 @@ const Expenses = () => {
         }
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       });
   }, []);
 
   const handleExpensesFormData = () => {
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        amount: amount.current.value,
-        desc: desc.current.value,
-        category: category.current.value,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => {
-        return res.json();
+    if (isEditing) {
+      const editURL =
+        "https://nice-theater-338718-default-rtdb.firebaseio.com/expenses/" +
+        id +
+        ".json";
+      fetch(editURL, {
+        method: "PUT",
+        body: JSON.stringify({
+          amount: formRefs.amount.current.value,
+          desc: formRefs.desc.current.value,
+          category: formRefs.category.current.value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .then((data) => {
-        const filteredData = [];
-        for (let key in data) {
-          filteredData.unshift({
-            ...data[key],
-            id: key,
+        .then((res) => {
+          return res.json();
+        })
+        .then((response) => {
+          const filteredData = [];
+          for (let key in response) {
+            filteredData.unshift({
+              ...response[key],
+              id: key,
+            });
+          }
+
+          setExpenseData((prevItems) =>
+            prevItems.map((item) =>
+              item.id === filteredData.id ? response.data : item
+            )
+          );
+          if (response.error) {
+            throw new Error(response.error.message);
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      if (
+        formRefs.amount.current &&
+        formRefs.desc.current &&
+        formRefs.category.current
+      ) {
+        fetch(url, {
+          method: "POST",
+          body: JSON.stringify({
+            amount: formRefs.amount.current.value,
+            desc: formRefs.desc.current.value,
+            category: formRefs.category.current.value,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            const filteredData = [];
+            for (let key in data) {
+              filteredData.unshift({
+                ...data[key],
+                id: key,
+              });
+            }
+            setExpenseData(filteredData);
+            if (data.error) {
+              throw new Error(data.error.message);
+            }
+          })
+          .catch((error) => {
+            console.error(error);
           });
-        }
-        setExpenseData(filteredData);
-        if (data.error) {
-          throw new Error(data.error.message);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      }
+    }
   };
 
   const deleteExpensesHandler = (id) => {
-    console.log(id);
     const deleteURL =
       "https://nice-theater-338718-default-rtdb.firebaseio.com/expenses/" +
       id +
@@ -90,9 +146,19 @@ const Expenses = () => {
       });
   };
 
-  console.log(expenseData);
-  const editExpenseHandler = (id) => {
-    console.log(id);
+  const editExpenseHandler = (item) => {
+    setIsEditing(true);
+    setId(item.id);
+
+    if (
+      formRefs.amount.current &&
+      formRefs.desc.current &&
+      formRefs.category.current
+    ) {
+      formRefs.amount.current.value = item.amount;
+      formRefs.desc.current.value = item.desc;
+      formRefs.category.current.value = item.category;
+    }
   };
 
   return (
@@ -112,18 +178,18 @@ const Expenses = () => {
             type="number"
             placeholder="amount"
             className="m-4 p-2 rounded-lg"
-            ref={amount}
+            ref={formRefs.amount}
           />
           <input
             type="text"
             placeholder="description"
             className="m-4 p-2 py-4 rounded-lg"
-            ref={desc}
+            ref={formRefs.desc}
           />
           <select
             className="m-4 p-2 px-16  rounded-lg"
             placeholder="Select an option"
-            ref={category}
+            ref={formRefs.category}
           >
             <option value=""></option>
             <option value="petrol">Petrol</option>
@@ -151,7 +217,7 @@ const Expenses = () => {
                   {expense.amount} {expense.desc} {expense.category}
                   <button
                     className="p-2 m-2 bg-slate-800 text-white rounded-lg"
-                    onClick={() => editExpenseHandler(expense.id)}
+                    onClick={() => editExpenseHandler(expense)}
                   >
                     Edit Expense
                   </button>
