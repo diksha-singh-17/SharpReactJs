b4bc476ef8e7d611fbb3c4ea66bc9613a0ef529a
Diksha Singh <dikshasinghrathore1107@gmail.com>
2024-07-20 19:21:47 +0530

    code cleanup

--- nextjs-app/app/aboutus/[newId]/page.js
+++ nextjs-app/app/aboutus/[newId]/page.js
@@ -1,16 +1,12 @@
 import React from "react";
-// import { useRouter } from "next/router";
 
 const Detailpage = ({ params }) => {
-  // const router = useRouter();
-
   const details = [
     { id: 1, name: "Yash", role: "Senior Developer" },
     { id: 2, name: "Vaibhav", role: "Backend Developer" },
     { id: 3, name: "Suresh", role: "Frontend Developer" },
   ];
   const id = params.newId;
-  console.log(params.newId, "idddddddddddddd", details[id]);
   return (
     <>
       {!details[id] ? (
