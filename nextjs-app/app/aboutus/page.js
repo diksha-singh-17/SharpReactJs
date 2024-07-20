cd07c0a214aab8791586d29f669df4a38a16b662
Diksha Singh <dikshasinghrathore1107@gmail.com>
2024-07-20 19:44:37 +0530

    Final touch to About US page

--- nextjs-app/app/aboutus/page.js
+++ nextjs-app/app/aboutus/page.js
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div>Hi from AboutUs page</div>;
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+
+  return (
+    <div>
+      <h1>Details</h1>
+      <ul>
+        {details.map((detail) => (
+          <li key={detail.id}>
+            <Link href={`aboutus/${detail.id - 1}`}>{detail.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default page;
