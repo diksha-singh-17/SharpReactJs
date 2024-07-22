ca30fdfbac7ab65faac9fd9d11be3264b1de95b9
Diksha Singh <dikshasinghrathore1107@gmail.com>
2024-07-22 19:37:33 +0530

    code cleanup

--- nextjs-app/app/events/page.js
+++ nextjs-app/app/events/page.js
@@ -1,12 +1,16 @@
+"use client";
 import Layout from "@/components/layout/Layout";
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
 import React from "react";
 
 const page = () => {
+  function addMeetupHandler(meetupData) {
+    console.log(meetupData);
+  }
   return (
     <div>
       <Layout>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
       </Layout>
     </div>
   );
