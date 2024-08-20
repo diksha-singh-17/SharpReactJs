9a9d62c9336f39c5b38d92b4db54e238f64a59a3
diksha-singh-17 <dikshasinghrathore1107@gmail.com>
2024-08-20 21:38:52 +0530

    upperbound & lowerbound implementation

--- code/PractisingDSA/BinarySearch/LowerBound/index.js
+++ code/PractisingDSA/BinarySearch/LowerBound/index.js
@@ -1,18 +1,3 @@
-// function Upperbound(nums, target, n) {
-//   let left = 0;
-//   let right = n - 1;
-//   let ans = n;
-//   while (left <= right) {
-//     let mid = Math.floor((left + right) / 2);
-//     if (nums[mid] <= target) {
-//       left = mid + 1;
-//     } else {
-//       right = mid - 1;
-//       ans = mid;
-//     }
-//   }
-//   return ans;
-// }
 var upperBound = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
@@ -21,7 +6,7 @@ var upperBound = function (nums, target) {
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
     // console.log(mid, target);
-    if (nums[mid] > target) {
+    if (nums[mid] >= target) {
       // console.log("ans", mid);
       ans = mid;
       right = mid - 1;
